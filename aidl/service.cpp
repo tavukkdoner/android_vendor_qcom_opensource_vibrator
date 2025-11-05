d4e4ec11899bb68a62bd1e469ba2f1d4180634fa
Kisan Yadav <kisany@qti.qualcomm.com>
2025-11-05 11:08:13 +0530

    vibratorCL: update HapticsState at start of stop stream
    Binder trasaction happens from vibrator service to vibrator HAL and then it can go to vibratorOL or vibratorCL depending on the usecase triggered.
    When OL usecase is triggered, CL is supposed to be stopped. From vibrator HAL CL is closed first. There is a possibility that CL is already addressing stop and in that case when we are trying to teardown CL from OL context is not supposed to be done as CL is already tearing down.
    Updating HapticsState at start of stop to make sure other caller can use the flag before calling stop.
    
    Change-Id: I787d789c564e5b1a37e8b14e46aab0734e288469

--- aidl/service.cpp
+++ aidl/service.cpp
@@ -26,6 +26,7 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ * ​​​​​Changes from Qualcomm Technologies, Inc. are provided under the following license:
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
